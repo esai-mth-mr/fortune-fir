32867727abf687466a6a91920b19e240ddddd729
unknown <esaibrilliant34310@gmail.com>
2024-12-12 15:41:05 -0800

    add animation to homepage

---
+++ src/helper/Helper.ts
@@ -0,0 +1,13 @@
+export const getRandomNum = (from: number, to: number, num: number): number[] | null => {    
+    if (to < from || num <= 0 || !Number.isInteger(num)) return null;
+    return Array.from({ length: num }, () => Math.random() * (to - from) + from);
+};
+
+export const backgroundImages = [
+    '/src/assets/bg-1.png',
+    '/src/assets/bg-2.png',
+    '/src/assets/bg-3.png',
+    '/src/assets/bg-4.png',
+    '/src/assets/bg-5.png',
+    '/src/assets/bg-6.png'
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,47 @@
-import '@src/style/global.scss'
+import '@src/style/pages/home.scss';
+import '@src/style/global.scss';
+import { useEffect, useState } from 'react';
+import { backgroundImages } from '../helper/Helper';
 
 function Home() {
+    const [currentImage, setCurrentImage] = useState<string>(backgroundImages[0]);
+    const [nextImage, setNextImage] = useState<string>(backgroundImages[1]);
+    const [fadeOut, setFadeOut] = useState<boolean>(false);
+    const [imageKey, setImageKey] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFadeOut(true);
+
+            setTimeout(() => {
+                setImageKey((prev) => prev + 1);
+                setNextImage(backgroundImages[(imageKey + 1) % backgroundImages.length]);
+                setFadeOut(false);
+            }, 2000);
+
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [imageKey]);
 
     return (
-        <>
-            <div className="board">
-
+        <div className="board">
+            <div className='img'>
+                <img
+                    src={currentImage}
+                    alt="Current Background"
+                    className={fadeOut ? 'fade-out' : 'fade-in'}
+                />
+                <img
+                    src={nextImage}
+                    alt="Next Background"
+                    className={!fadeOut ? 'fade-in' : 'fade-out'}
+                />
             </div>
-        </>
-    )
+            <img className='title' src="src/assets/title.png" />
+            <img className='logo' src="src/assets/logo.png" />
+        </div>
+    );
 }
 
-export default Home
+export default Home;
