417a29378571ba5dc6d1e0e6b07723dfe42b5978
superdragonstar0120 <MarkWindsor0120@outlook.com>
2024-12-22 19:09:34 -0800

    12/22/modal_update_01

--- src/pages/modal/modal.tsx
+++ src/pages/modal/modal.tsx
@@ -25,6 +25,7 @@ const Modal: React.FC<ModalProps> = ({ setIsOpen, score, modalData }) => {
     <>
       <div className="modal">
         <img onClick={() => {
+<<<<<<< Updated upstream
             allowclose == true && setIsOpen(false);
             setAllowClose(false);
           }}
@@ -32,6 +33,24 @@ const Modal: React.FC<ModalProps> = ({ setIsOpen, score, modalData }) => {
           src="assets/close.png" 
           draggable={false} 
           alt="modal_close"/>
+=======
+            allowclose == true && setIsOpen(false);
+            setAllowClose(false);
+          }}
+          className="modal_close" 
+          src="assets/close.png" 
+          draggable={false} 
+          alt="modal_close"/>
+        {/* <div
+          className="modal_close"
+          onClick={() => {
+            allowclose == true && setIsOpen(false);
+            setAllowClose(false);
+          }}
+        >
+          X
+        </div> */}
+>>>>>>> Stashed changes
         <div className="modal_darkbg" />
         <img
           className="modal_gift"
