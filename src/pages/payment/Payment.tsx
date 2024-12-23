1b316692adaf162b3dd241b65235f53fb8c00dfb
undyingkevin <williamadams19901111@gmail.com>
2024-12-23 06:58:48 -0500

    stripe

--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -131,6 +131,8 @@ export default function Payment(props: {
             width: "280px",
             height: "470px",
             backgroundColor: "#f5f5f5",
+            boxShadow:
+              "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px !important",
           },
         }}
         TransitionComponent={Transition}
