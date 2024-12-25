276e9ab97a0496654cba7172b3ba39ca90f18913
SecretCodingStar <t56200840@gmail.com>
2024-12-24 22:04:50 -0500

    insert getready page

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
-// const BACKEND_URL = "https://howlucky2025.com";
+const BACKEND_URL = "https://howlucky2025.com";
 // const BACKEND_URL = "http://localhost:8000";
-const BACKEND_URL = "http://54.167.191.81:8000";
+// const BACKEND_URL = "http://54.167.191.81:8000";
 const TOKEN_SECRET_KEY = "aceron20030612";
 const ERRORS = {
     getDataError: "You have already processed all months",
